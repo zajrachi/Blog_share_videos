0f4a3f08d9ac9fefaf4929a6108a3ab25846df8d
zajrachi <boygiaothuypro@gmail.com>
2021-05-11 15:33:55 +0700

    update 11/05/2021

---
+++ key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    GOOGLE_CLIENT_ID : '275162454535-v1odsqicv7kjvda7haold66nik3qo1lh.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET : 'wUlTq2lQ1mIdJI-ow1vnf6vE',
+    FACEBOOK_CLIENT_ID : '227546949151321',
+    FACEBOOK_CLIENT_SECRET : '58e86b80cddc95d610961eee6cfd26a1'
+}
+  
